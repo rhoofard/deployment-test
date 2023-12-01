8fb06ec5a19187b18e0afb8abaf123c0bc2209dd
Ryan Hoofard <42755382+rhoofard@users.noreply.github.com>
2023-12-01 00:00:17 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+resource "azurerm_public_ip" "example" {
+  name                = "PublicIp1"
+  resource_group_name = "ryanh-workflow-test"
+  location            = "East US"
+  allocation_method   = "Static"
+
+  tags = {
+    Owner = "ryanh"
+  }
+}
