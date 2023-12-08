4913b4de35d97be65f39b902daba8baeee771ea3
Ryan Hoofard <42755382+rhoofard@users.noreply.github.com>
2023-12-08 10:27:41 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "example" {
   name                = "PublicIp1"
   resource_group_name = "ryanh-workflow-test"
-  location            = "East US"
+  location            = "broke"
   allocation_method   = "Static"
 
   tags = {
