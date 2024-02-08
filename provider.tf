db4dca180862755b1f8d73721bda47bef16703ca
Ryan Hoofard <42755382+rhoofard@users.noreply.github.com>
2024-02-08 08:08:09 -0800

    use idc

--- provider.tf
+++ provider.tf
@@ -13,9 +13,11 @@ terraform {
     storage_account_name = "ryanhterraform"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
+    use_oidc = true
   }
 }
 
 provider "azurerm" {
+  use_oidc = true
   features {}
 }
